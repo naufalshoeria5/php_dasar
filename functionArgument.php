6a5af271a0a07ca9d3700fd30d5ae23b4aca3656
naufalshoeria5 <naufalhidayah17@gmail.com>
2023-06-14 00:50:12 +0700

    function argument

--- functionArgument.php
+++ functionArgument.php
@@ -1,13 +1,50 @@
 <?php
-// function in if statement
 
-$condition = true;
-
-if ($condition) {
-    function hi($name)
-    {
-        echo "Hai $name";
-    }
+// function argument
+function hi($name)
+{
+    echo "Hai $name";
 }
 
 hi("naufal");
+
+// default argument value
+function sayHello($name = 'ga ngasih nama')
+{
+    echo "hai $name";
+}
+
+sayHello();
+
+// type declaration argument
+function sayHelloType(int $name)
+{
+    echo "hai $name";
+}
+
+sayHelloType(10);
+
+// * note valid type
+// class parameter harus tipe class
+// self parameter harus sama dengan class dimana function ni dibuat
+// array parameter harus array
+// callable parameter harus callable
+// bool paramter harus boolean
+// float paramter harus floating point
+// int parameter harus integer
+// string paramenter harus string
+// interable parameter harus array atau Traversable
+// object parameter harus object
+
+// *
+
+
+function sum(int $first, int $second)
+{
+    $total = $first + $second;
+    echo "Total = $total" . PHP_EOL;
+}
+
+sum("10", 10);
+sum(10, 10);
+sum(true, false);
